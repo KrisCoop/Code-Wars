b65cec497a1e751b9c29a56b82ad4c5de21da9b7
KrisCoop <47994388+KrisCoop@users.noreply.github.com>
2019-03-04 18:04:45 -0700

    code wars monday Mar4

--- world.js
+++ world.js
@@ -1,1 +1,6 @@
-var bike = cool;
+var couch = {
+    cushions: looseChange,
+    fabric: ninetiesDamaskPattern,
+    stains: "It's stains are many and magnificent",
+    history: "Shrouded in mystery",
+};
